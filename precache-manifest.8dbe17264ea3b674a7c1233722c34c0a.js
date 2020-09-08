c3fb6e13d958d0a53c4cfd7792ae69549e89a8c0
jking <jking@esri.com>
2020-09-08 17:19:27 -0500

    Updates

---
+++ precache-manifest.8dbe17264ea3b674a7c1233722c34c0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5eddebc90e03e96038ce8b967a4c23b",
+    "url": "/race-relations-word-cloud/index.html"
+  },
+  {
+    "revision": "61c665c477971e0cda65",
+    "url": "/race-relations-word-cloud/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5dcce8b07741f115c482",
+    "url": "/race-relations-word-cloud/static/css/main.90be11e0.chunk.css"
+  },
+  {
+    "revision": "61c665c477971e0cda65",
+    "url": "/race-relations-word-cloud/static/js/2.fb0f8da9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/race-relations-word-cloud/static/js/2.fb0f8da9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dcce8b07741f115c482",
+    "url": "/race-relations-word-cloud/static/js/main.275297be.chunk.js"
+  },
+  {
+    "revision": "f747869f13e0a4be00d0",
+    "url": "/race-relations-word-cloud/static/js/runtime-main.fe37264b.js"
+  }
+]);
